fd44560f472a707720b1782709ae023f050e9059
Bill Liu <liu_bill@live.com>
2015-02-16 23:07:31 -0600

    billliu2 ngallow2 - Changing Angular so that only one service call is required, did more layout

--- dash/index.php
+++ dash/index.php
@@ -1,11 +1,12 @@
 <?php require($_SERVER['DOCUMENT_ROOT'] . "/php/include/first.php"); ?>
 <!DOCTYPE html>
 
-<html ng-app="homepageApp">
+<html ng-app="homepageApp" ng-controller="homepageController">
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width" />
 
-		<link rel="stylesheet" href="/css/homepage.css" type="text/css">
+		<title ng:bind-template="homepage - {{hpUser.firstName}} {{hpUser.lastName}}">homepage</title>
 
 		<!-- USING CDN FOR NOW, CHANGE LATER -->
 		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular.js"></script>
@@ -14,10 +15,10 @@
 		<script src="/js/modules/homepage.js"></script>
 		<script src="/js/modules/feed.js"></script>
 		<script src="/js/modules/search.js"></script>
-		<script src="/js/modules/quickbar.js"></script>
 
 		<?php require($_SERVER['DOCUMENT_ROOT'] . "/php/include/head.php"); ?>
-		
+
+		<link rel="stylesheet" href="/css/homepage.css" type="text/css">
 	</head>
 	<body>
 	
@@ -27,46 +28,59 @@
 		<a href="#/dash/search">Search</a>
 		<a href="/php/scripts/logout.php">Log Out</a>
 		<span>This here for debug</span>
-
-		<div id="top-bar">
-			<div id="logo" class="top-bar-item">Logo Here</div><!--
-		--><div id="quickbar" class="top-bar-item" ng-controller="homepageQuickbarController">
-				<ul id="quickbar-list">
-					<li class="quickbar-item" ng-repeat="quickbarItem in quickbarItems">
-						<a href="{{quickbarItem.url}}">
-							<img ng-src="{{quickbarItem.iconUrl}}" title="{{quickbarItem.title}}" />
-						</a>
-					</li>
-				</ul>
-			</div><!--
-		--><div id="black-bar-wrapper" class="top-bar-item"><!--
-		--><div id="user-settings">
-					<span id="user-welcome">Welcome <a id="user-name">Matt</a></span>
-					<span id="pref-btn">PRF<!-- Replace me --></span>
-					<span id="power-btn">PRW<!-- Replace me --></span>
+		<div id="homepage-wrapper">
+			<div id="top-bar">
+				<div id="logo" class="top-bar-item">Logo Here</div><!--
+			 --><div id="quickbar" class="top-bar-item">
+					<ul id="quickbar-list">
+						<li class="quickbar-item" ng-repeat="quickbarItem in hpUser.quickbarItems">
+							<a href="{{quickbarItem.url}}">
+								<img ng-src="{{quickbarItem.iconUrl}}" title="{{quickbarItem.title}}" />
+							</a>
+						</li>
+					</ul>
+				</div><!--
+			 --><div id="black-bar-wrapper" class="top-bar-item">
+			 		<div id="user-settings">
+						<span id="user-welcome">Welcome <a id="user-name">{{ hpUser.firstName }}</a></span>
+						<span id="pref-btn">PRF<!-- Replace me --></span>
+						<span id="power-btn">PRW<!-- Replace me --></span>
+					</div>
 				</div>
 			</div>
-		</div>
-		
-		<div id="categories">
-			<span id="add-more-cats" class="category-item"></span>
-		</div>
-
-		<div id="homepage-main" ng-view></div>
-
-		<div id="bottom-bar">
-
-			<div id="widget-1" class="widget"></div>
-			<div id="widget-2" class="widget"></div>
-			<div id="widget-3" class="widget"></div>
 			
-			<div id="info">
-				<div id="info-weather">
-					<span>It's snowing</span>
+			<div id="categories-wrapper">
+				<div id="categories">
+					<span id="add-more-cats" class="category-item">
+						<span class="category-text">+</span>
+					</span>
+					<!--  Placeholder -->
+					<span class="category-item">Test Item 1</span>
+					<span class="category-item">Test Item 2</span>
+					<span class="category-item">Test Item 3</span>
 				</div>
-				<div id="location-info">
-					<span>Champaign, IL</span>
-					<span>13:00</span>
+			</div>
+
+			<div id="search-section">
+				<input type="text" id="search-input" placeholder="Search" />
+			</div>
+			<div id="homepage-main" ng-view></div>
+
+			<div id="bottom-bar">
+				<div id="widgets-area" class="bottom-bar-item">
+					<div id="widget-1" class="widget"></div>
+					<div id="widget-2" class="widget"></div>
+					<div id="widget-3" class="widget"></div>
+				</div>
+
+				<div id="info" class="bottom-bar-item">
+					<div id="info-weather">
+						<span>It's snowing</span>
+					</div>
+					<div id="location-info">
+						<span>Champaign, IL</span>
+						<span>13:00</span>
+					</div>
 				</div>
 			</div>
 		</div>
--- js/modules/homepage.js
+++ js/modules/homepage.js
@@ -3,8 +3,34 @@
  * Also holds the config and routes for the website
  */
 
-var homepageApp = angular.module("homepageApp", ["ngRoute", "homepageFeed", "homepageSearch", "homepageQuickbar"]);
+var quickbarApiUrl = "/dash/fake_api/fake_quickbar.json";	//MODIFY THIS TO THE CORRECT URL TO RETRIEVE THE QUICKBAR
 
+var homepageApp = angular.module("homepageApp", ["ngRoute", "homepageFeed", "homepageSearch"]);
+
+homepageApp.controller("homepageController", ["$scope", "userService",
+	function($scope, userService) {
+		userService(function(data) {
+			$scope.hpUser = data;
+		});
+	}]
+);
+
+/* This service returns a function that takes a callback, this callback is called when the data is successfully retreived from API 
+ */
+homepageApp.factory("userService", ["$http",
+	function($http) {
+		return function(callback) {
+			//Build the URL, since we are using a GET request
+			var urlWithParams = quickbarApiUrl + "?sessionId=" + "0";	
+			$http.get(urlWithParams).success(function(data) {
+				callback(data);
+			});
+		}
+	}]
+);
+
+
+/* Routes */
 homepageApp.config(["$routeProvider",
 	function($routeProvider) {
 		$routeProvider
