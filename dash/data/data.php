29927548b0c710d9bd9c1d08a24ef35a08b0392b
Bill Liu <liu_bill@live.com>
2015-03-11 23:01:18 -0500

    Finishing layout for feeds

--- dash/data/data.php
+++ dash/data/data.php
@@ -64,12 +64,99 @@
 	],
 	"feed" : [
 		{
+			"url" : "http://www.reddit.com/r/aww",
 			"classes" : "feed-item-big",
-			"imgSrc" : "",
-			"iconSrc" : "",
-			"time" : "",
-			"title" : "",
-			"author" : ""
+			"imgSrc" : "http://i.imgur.com/ujkWhhV.jpg",
+			"iconSrc" : "/img/icons/02_facebook.png",
+			"time" : "1:00 PM",
+			"title" : "Check this cool link out",
+			"author" : "John Smith"
+		},
+		{
+			"url" : "http://www.google.com",
+			"classes" : "feed-item-small",
+			"imgSrc" : "http://i.imgur.com/bOd2iVK.jpg",
+			"iconSrc" : "/img/icons/02_facebook.png",
+			"time" : "2:00 PM",
+			"title" : "Test title",
+			"author" : "Me"
+		},
+		
+		{
+			"url" : "http://www.google.com",
+			"classes" : "feed-item-small",
+			"imgSrc" : "http://i.imgur.com/bOd2iVK.jpg",
+			"iconSrc" : "/img/icons/02_facebook.png",
+			"time" : "3:00 PM",
+			"title" : "Test title",
+			"author" : "Me"
+		},
+		{
+			"url" : "http://www.google.com",
+			"classes" : "feed-item-big",
+			"imgSrc" : "http://i.imgur.com/UC8srbW.jpg",
+			"iconSrc" : "/img/icons/02_facebook.png",
+			"time" : "4:00 PM",
+			"title" : "A Corgi",
+			"author" : "Me"
+		},
+		
+		{
+			"url" : "http://www.google.com",
+			"classes" : "feed-item-tiny",
+			"imgSrc" : "http://i.imgur.com/bOd2iVK.jpg",
+			"iconSrc" : "/img/icons/02_facebook.png",
+			"time" : "5:00 PM",
+			"title" : "Test title",
+			"author" : "Me"
+		},
+		{
+			"url" : "http://www.google.com",
+			"classes" : "feed-item-tiny",
+			"imgSrc" : "http://i.imgur.com/bOd2iVK.jpg",
+			"iconSrc" : "/img/icons/02_facebook.png",
+			"time" : "6:00 PM",
+			"title" : "Test title",
+			"author" : "Me"
+		},
+		{
+			"url" : "http://www.google.com",
+			"classes" : "feed-item-tiny",
+			"imgSrc" : "http://i.imgur.com/bOd2iVK.jpg",
+			"iconSrc" : "/img/icons/02_facebook.png",
+			"time" : "7:00 PM",
+			"title" : "Test title",
+			"author" : "Me"
+		},
+		
+		{
+			"url" : "http://www.google.com",
+			"classes" : "feed-item-big",
+			"imgSrc" : "http://i.imgur.com/bOd2iVK.jpg",
+			"iconSrc" : "/img/icons/02_facebook.png",
+			"time" : "8:00 PM",
+			"title" : "Test title",
+			"author" : "Me"
+		},
+		{
+			"url" : "http://www.google.com",
+			"classes" : "feed-item-small",
+			"imgSrc" : "http://i.imgur.com/bOd2iVK.jpg",
+			"iconSrc" : "/img/icons/02_facebook.png",
+			"time" : "9:00 PM",
+			"title" : "Test title",
+			"author" : "Me"
+		},
+		
+		{
+			"url" : "https://www.youtube.com/watch?v=J---aiyznGQ",
+			"classes" : "feed-item-giant",
+			"imgSrc" : "https://i.ytimg.com/vi/J---aiyznGQ/hqdefault.jpg",
+			"iconSrc" : "/img/icons/03_youtube.png",
+			"time" : "10:00 PM",
+			"title" : "Charlie Schmidt's Keyboard Cat! - THE ORIGINAL! ",
+			"author" : "chuckieart"
+		}
 	]
 }
 
