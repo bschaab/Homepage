9dc6103626ae317c523ad2386171c62b05267fa3
Bill Liu <liu_bill@live.com>
2015-04-23 19:01:29 -0500

    Fixing Twitter feed for new users

--- php/models/feed/TwitterFeed.php
+++ php/models/feed/TwitterFeed.php
@@ -6,8 +6,8 @@
 	
 	class TwitterFeed implements ExternalFeed {
 	
-		private $twitter_obj;
-	
+		private $twitter_obj;	
+
 		public function __construct($userId) {
 			$twitterDB = new TwitterDB();
 			$twitterDB->loadTokens($userId);
@@ -15,52 +15,59 @@
 			$oauth_token_secret = $twitterDB->getOauthTokenSecret();
 			$username = $twitterDB->getUsername();
 
-			/*set access tokens */
-			$settings = array(
-				'oauth_access_token' => $oauth_token,
-				'oauth_access_token_secret' => $oauth_token_secret,
-				'consumer_key' => "lv9iiNeCkmBOX523sOqy5BvLI",
-				'consumer_secret' => "2OSfTohYKDc338orDKT7KzwmuRbctpP65riLFgURLwl9xAn2x5"
-			);
+			if($oauth_token === "" || $oauth_token === null) {
+				$this->twitter_obj = null;	
+			}
+			else {
+				/*set access tokens */
+				$settings = array(
+					'oauth_access_token' => $oauth_token,
+					'oauth_access_token_secret' => $oauth_token_secret,
+					'consumer_key' => "lv9iiNeCkmBOX523sOqy5BvLI",
+					'consumer_secret' => "2OSfTohYKDc338orDKT7KzwmuRbctpP65riLFgURLwl9xAn2x5"
+				);
 
-			$requestMethod = "GET";
+				$requestMethod = "GET";
 
-			$getfield = '?screen_name='.$username.'&count=10';
+				$getfield = '?screen_name='.$username.'&count=10';
 
-			$this->twitter_obj = new TwitterAPIExchange($settings);
+				$this->twitter_obj = new TwitterAPIExchange($settings);
+			}
 			
 		}
 		
 		public function getFeedItems() {
-			$result_str = $this->twitter_obj->setGetfield('?count=10')->buildOauth("https://api.twitter.com/1.1/statuses/home_timeline.json", "GET")->performRequest();
-		
-			$twitter_objs = json_decode($result_str, true);
+			if($this->twitter_obj !== null) {
+				$result_str = $this->twitter_obj->setGetfield('?count=10')->buildOauth("https://api.twitter.com/1.1/statuses/home_timeline.json", "GET")->performRequest();
 			
-			$feed_objs = array();
-			
-			forEach($twitter_objs as $feed_obj) {
-				$image_url = "";
-			
-				$feed_entities = $feed_obj["entities"];
-				if(in_array("media", $feed_entities)) {
-					$image_url = $feed_entities["media"][0]["media_url"];
-				}
-				else {
-					$image_url = $feed_obj["user"]["profile_banner_url"];
+				$twitter_objs = json_decode($result_str, true);
+				
+				$feed_objs = array();
+				
+				forEach($twitter_objs as $feed_obj) {
+					$image_url = "";
+				
+					$feed_entities = $feed_obj["entities"];
+					if(in_array("media", $feed_entities)) {
+						$image_url = $feed_entities["media"][0]["media_url"];
+					}
+					else {
+						$image_url = $feed_obj["user"]["profile_banner_url"];
+					}
+					
+					$feed_objs[] = new FeedItem(
+						$feed_obj["text"],
+						$feed_obj["user"]["name"],
+						$feed_obj["created_at"],
+						"http://twitter.com/" . $feed_obj["user"]["screen_name"] . "/status/" . $feed_obj["id"],
+						"/img/icons/01_twitter.png",
+						$image_url,
+						1
+					);
 				}
 				
-				$feed_objs[] = new FeedItem(
-					$feed_obj["text"],
-					$feed_obj["user"]["name"],
-					$feed_obj["created_at"],
-					"http://twitter.com/" . $feed_obj["user"]["screen_name"] . "/status/" . $feed_obj["id"],
-					"/img/icons/01_twitter.png",
-					$image_url,
-					1
-				);
+				return $feed_objs;
 			}
-			
-			return $feed_objs;
 		}
 	}
 ?>
